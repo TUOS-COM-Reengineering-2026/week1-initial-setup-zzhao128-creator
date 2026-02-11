5a4fad728d350fe36b47d5b3dc806337d1853893
zz23120 <zz23120@essex.ac.uk>
2026-02-11 04:00:35 +0000

    Change the print message

--- tests/test_autograte.py
+++ tests/test_autograte.py
@@ -1,4 +1,7 @@
 import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import hello
 
 
@@ -6,4 +9,3 @@ class TestAutograder(unittest.TestCase):
     def test_result(self):
         # check if the file `causal_testing.png` exists
         self.assertEqual("Hello Reengineering", hello.say_hello())
-
